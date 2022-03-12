ee0364096c6ab25f88f5fb54a6e1ff9edcf2e408
zebulon75018 <zebulon75018@yahoo.fr>
2022-03-13 00:30:48 +0100

    checkout the good version of qdocker...

--- compil.sh
+++ compil.sh
@@ -1,17 +1,21 @@
 git clone  https://github.com/githubuser0xFFFF/Qt-Advanced-Docking-System
 cd Qt-Advanced-Docking-System
+git checkout tags/3.7.0
 qmake
 make
+############
 cd ..
 git clone  https://github.com/jacob3141/qtterminalwidget
 cd qtterminalwidget
 qmake 
 make
+############
 cd ..
 git clone https://github.com/Skycoder42/QSingleInstance
 cd QSingleInstance
 qmake
 make
+############
 cd ..
 qmake
 make
